4357479eadfc805c2200dead45b474f387e1c99e
rayesyounes <rayesyounes88@gmail.com>
2024-04-19 00:25:50 +0100

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" _summary_ """
+
+import math
+
+
+def floor(n: float) -> int:
+    """_summary_
+    Args:
+        n (float): float
+    Returns:
+        floar: int
+    """
+
+    return math.floor(n)
+
+
+ans = floor(3.14)
