51c32a9eac2ecb0d9043aa40231734dbd12ed9d8
rayesyounes <rayesyounes88@gmail.com>
2024-04-19 00:28:56 +0100

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" _summary_ """
+
+
+def to_str(n: float) -> str:
+    """_summary_
+    Args:
+        n (float): float
+    Returns:
+        str: string
+    """
+
+    return str(n)
