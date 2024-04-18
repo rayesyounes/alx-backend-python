27a9d59db47bf1567e22e9bf6d8a2517f9b41cd1
rayesyounes <rayesyounes88@gmail.com>
2024-04-19 00:11:56 +0100

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" _summary_ """
+
+
+def concat(str1: str, str2: str) -> str:
+    """_summary_
+    Args:
+        str1 (string): string
+        str2 (string): string
+    Returns:
+        string: a concatenated string
+    """
+    return str1 + str2
