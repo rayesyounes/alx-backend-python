89e24d222f2f0d7890b0eeb09028b25a11f5a77f
rayesyounes <rayesyounes88@gmail.com>
2024-04-18 23:58:54 +0100

    Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" _summary_"""
+
+
+def add(a: float, b: float) -> float:
+    """_summary_
+    Args:
+        a (float): _description_
+        b (float): _description_
+    Returns:
+        float: _description_
+    """
+    return a + b
