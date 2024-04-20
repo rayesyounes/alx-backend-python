289ae14897da20b23d1a22ddb98632fc7b8843eb
rayesyounes <rayesyounes88@gmail.com>
2024-04-20 23:59:29 +0100

    str, int, float to tuple

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" doc """
+
+from typing import List, Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """  doc """
+
+    return (k, v ** 2)
