42300b25740be0a850d972372a5cb7c4ab03d29a
rayesyounes <rayesyounes88@gmail.com>
2024-04-24 00:01:44 +0100

    fix type annotation

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,11 +2,11 @@
 """ doc """
 
 import random
-from typing import Any
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> list[Any]:
+async def async_comprehension() -> List[float]:
     """ doc """
     return [i async for i in async_generator()]
